dc7b1ca2ec900a47d4111e201e06c7100cdb38fd
rivermanvr <vrgithub@riversconsulting.com>
2018-02-27 15:17:13 -0500

    simple input and output

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UserOut from './UserOut/UserOut';
 
 class App extends Component {
   state = {
-    userId: 'Vin',
+    userId: '',
     paragraph_1: 'It was the best of times.  It was the worst of times.  I feel that these words can describe all spans of time in the history of man.',
     paragraph_2: 'Rows and flows of angel hair; And ice cream castles in the air; And feather canyons everywhere; Ive looked at clouds that way.  But now they only block the sun; They rain and snow on everyone; So many things I would have done; But clouds got in my way.  Ive looked at clouds from both sides now; From up and down and still somehow; Its clouds illusions I recall.  I really dont know clouds at all'
   };
@@ -23,7 +23,7 @@ class App extends Component {
           <h1 className="App-title">React-16-Example #1</h1>
         </header>
         <UserIn id={ this.state.userId } onInputChange={ this.onInputChange.bind(this) } />
-        <UserOut />
+        <UserOut p1={ this.state.paragraph_1 } p2={ this.state.paragraph_2 } />
       </div>
     );
   }
--- src/UserOut/UserOut.js
+++ src/UserOut/UserOut.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './UserOut.css';
 
-const userOut = () => {
+const userOut = ({ p1, p2 }) => {
+  const styleLine = {
+    marginTop: '20px',
+    marginBottom: '20px'
+  };
+  const styleOutput = {
+    height: 'auto',
+    width: '400px',
+    marginBottom: '30px',
+    fontSize: '20px',
+    textAlign: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  };
   return (
     <div>
-      UserOut component
+      <div style={ styleLine }><strong>Paragraphs</strong></div>
+      <div><p style={ styleOutput }>{ p1 }</p></div>
+      <div><p style={ styleOutput }>{ p2 }</p></div>
     </div>
-  )
-}
+  );
+};
 
 export default userOut;
